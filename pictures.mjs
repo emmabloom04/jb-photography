1cf5a5e36007137c249f216c94685b4541b1d6d0
emmabloom04 <blo23006@byui.edu>
2024-12-07 18:38:47 -0700

    doing lots of work for the website, adding lots of images and doing style

--- pictures.mjs
+++ pictures.mjs
@@ -1,61 +1,61 @@
 const pictures = [
     {
-        src: "images/aesthetic.png",
+        src: "images/aesthetic-square.jpeg",
         alt: "glass decor",
-        tags: ["wedding", "decor", "outside", "green"]
+        tags: ["wedding", "decor", "outside", "green", "glass"]
     },
     {
-        src: "images/engagements.png",
+        src: "images/engagements-square.jpeg",
         alt: "a couple",
         tags: ["engagements", "outside", "couple", "pink", "green", "spring"]
     },
     {
-        src: "images/family1.png",
+        src: "images/family1-square.jpeg",
         alt: "large family",
         tags: ["family", "outside", "winter", "snow", "green", "white"]
     },
     {
-        src: "images/family2.png",
+        src: "images/family2-square.jpeg",
         alt: "sisters",
         tags: ["family", "outside", "winter", "snow", "green", "white", "sisters", "girls"]
     },
     {
-        src: "images/family3.png",
+        src: "images/family3-square.jpeg",
         alt: "small family",
         tags: ["family", "baby", "couple", "winter", "outside", "snow", "green", "white"]
     },
     {
-        src: "images/grad1.png",
+        src: "images/grad1-square.jpeg",
         alt: "college graduate",
         tags: ["graduate", "college", "girl", "blue", "single", "outside"]
     },
     {
-        src: "images/grad2.png",
+        src: "images/grad2-square.jpeg",
         alt: "college graduate",
         tags: ["graduate", "college", "girl", "blue", "outside", "single", "jumping", "campus"]
     },
     {
-        src: "images/wedding1.png",
+        src: "images/wedding1-square.jpeg",
         alt: "wedding couple",
         tags: ["couple", "bride", "groom", "wedding", "outside", "green", "white", "blue"]
     },
     {
-        src: "images/wedding2.png",
+        src: "images/wedding2-square.jpeg",
         alt: "hands of a couple",
         tags: ["wedding", "couple", "bride", "groom", "ring", "closeup", "dress", "white"]
     },
     {
-        src: "images/wedding3.png",
+        src: "images/wedding3-square.jpeg",
         alt: "couple in front of a temple",
         tags: ["wedding", "couple", "temple", "white", "blue", "outside", "flowers", "bride", "groom"]
     },
     {
-        src: "images/wedding4.png",
+        src: "images/wedding4-square.jpeg",
         alt: "wedding couple",
         tags: ["wedding", "couple", "temple", "outside", "flowers", "white", "blue", "sun", "bride", "groom"]
     },
     {
-        src: "images/wedding5.png",
+        src: "images/wedding5-square.jpeg",
         alt: "bride with bridesmaids",
         tags: ["wedding", "bride", "white", "blue", "temple", "girls", "outside", "flowers"]
     }
