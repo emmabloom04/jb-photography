e4e413a2190e3797604b103d4d4f0826bf27ffce
emmabloom04 <blo23006@byui.edu>
2024-12-09 09:59:25 -0700

    making various changes, adding slideshow

--- main.mjs
+++ main.mjs
@@ -31,7 +31,25 @@ function filter(list, query) {
     return filtered;
 }
 
-console.log(document.title)
+let slideIndex = 0;
+if (document.title == "Josh Bloomfield Photography") {
+
+    showSlides();
+
+}
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slideshow-img");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
+
 if (document.title == "Portfolio") {
 
     renderPictures(pictures);
